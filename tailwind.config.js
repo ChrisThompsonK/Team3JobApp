b808d4c3b733c4e34fed1c45baa352929d1b4334
FinnMcCormack123 <finn.mccormack@kainos.com>
2025-10-01 12:01:58 +0100

    added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./views/**/*.{html,njk}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './views/**/*.{html,njk}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
