ebfb9ca726393e54a38c04657e637ca24ec1a51b
EmmaKainos <emma-louise.grier@kainos.com>
2025-11-04 13:17:26 +0000

    unused variable removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,6 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-const isCI = process.env.CI === 'true';
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
