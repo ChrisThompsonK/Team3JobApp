e341f59fd4aa2c53ba67a9faccb07f0a805a9c9c
Andrew Lyttle <andrew.lyttle@kainos.com>
2025-11-07 11:52:19 +0000

    remove unneccesssary if statements

--- tests/e2e/step_definitions/my-applications-steps.ts
+++ tests/e2e/step_definitions/my-applications-steps.ts
@@ -4,27 +4,22 @@ import { MyApplicationsPage } from '../pages/MyApplicationsPage';
 import type { CucumberWorld } from '../support/world';
 
 When('I click the My Applications button', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  this.myApplicationsPage = new MyApplicationsPage(this.page);
+  this.myApplicationsPage = new MyApplicationsPage(this.page!);
   await this.myApplicationsPage.clickMyApplicationsButton();
 });
 
 Then('I should see the applications page', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  await expect(this.page).toHaveURL(/\/my-applications/);
+  await expect(this.page!).toHaveURL(/\/my-applications/);
 });
 
 When('I click the back to home button', async function (this: CucumberWorld) {
-  if (!this.myApplicationsPage) throw new Error('MyApplicationsPage not initialized');
-  await this.myApplicationsPage.clickBackToHomeButton();
+  await this.myApplicationsPage!.clickBackToHomeButton();
 });
 
 When('I click the Kainos logo', async function (this: CucumberWorld) {
-  if (!this.myApplicationsPage) throw new Error('MyApplicationsPage not initialized');
-  await this.myApplicationsPage.clickKainosLogo();
+  await this.myApplicationsPage!.clickKainosLogo();
 });
 
 Then('I should be on the home page', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  await expect(this.page).toHaveURL(/\/$/);
+  await expect(this.page!).toHaveURL(/\/$/);
 });
