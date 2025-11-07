3bd2ca5a2f6016b26503947f9d83b7785bec9cf4
Andrew Lyttle <andrew.lyttle@kainos.com>
2025-11-07 13:45:43 +0000

    errors

--- tests/e2e/step_definitions/login-steps.ts
+++ tests/e2e/step_definitions/login-steps.ts
@@ -3,18 +3,15 @@ import { LoginPage } from '../pages/LoginPage';
 import type { CucumberWorld } from '../support/world';
 
 When('I log in as a regular user', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  this.loginPage = new LoginPage(this.page);
+  this.loginPage = new LoginPage(this.page!);
   await this.loginPage.loginAsRegularUser();
 });
 
 When('I log in as an admin user', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  this.loginPage = new LoginPage(this.page);
+  this.loginPage = new LoginPage(this.page!);
   await this.loginPage.loginAsAdmin();
 });
 
 Then('I should be redirected away from the login page', async function (this: CucumberWorld) {
-  if (!this.loginPage) throw new Error('LoginPage not initialized');
-  await this.loginPage.isNotOnLoginPage();
+  await this.loginPage!.isNotOnLoginPage();
 });
--- tests/e2e/step_definitions/report-steps.ts
+++ tests/e2e/step_definitions/report-steps.ts
@@ -5,13 +5,11 @@ import { ReportPage } from '../pages/ReportPage';
 import type { CucumberWorld } from '../support/world';
 
 When('I navigate to job listings', async function (this: CucumberWorld) {
-  if (!this.page) throw new Error('Page not initialized');
-  this.jobListingsPage = new JobListingsPage(this.page);
+  this.jobListingsPage = new JobListingsPage(this.page!);
   await this.jobListingsPage.navigateToJobListings();
 });
 
 Then('I should see the Report button', async function (this: CucumberWorld) {
-  if (!this.jobListingsPage) throw new Error('JobListingsPage not initialized');
-  const isVisible = await this.jobListingsPage.isReportButtonVisible();
+  const isVisible = await this.jobListingsPage!.isReportButtonVisible();
   expect(isVisible).toBe(true);
 });
