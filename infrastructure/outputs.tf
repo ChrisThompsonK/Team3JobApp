3f42ee712fa8641315fd9254a1d88c3522e7fd93
EmmaKainos <emma-louise.grier@kainos.com>
2025-11-19 12:24:27 +0000

    requested changes

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,34 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.main.name
+  description = "Resource group name"
+}
+
+output "resource_group_id" {
+  value       = azurerm_resource_group.main.id
+  description = "Resource group ID"
+}
+
+output "frontend_managed_identity_id" {
+  value       = azurerm_user_assigned_identity.frontend.id
+  description = "Frontend Managed Identity ID"
+}
+
+output "frontend_managed_identity_principal_id" {
+  value       = azurerm_user_assigned_identity.frontend.principal_id
+  description = "Frontend Managed Identity Principal ID"
+}
+
+output "frontend_container_app_id" {
+  value       = azurerm_container_app.frontend.id
+  description = "Frontend Container App ID"
+}
+
+output "frontend_container_app_name" {
+  value       = azurerm_container_app.frontend.name
+  description = "Frontend Container App name"
+}
+
+output "frontend_container_app_fqdn" {
+  value       = azurerm_container_app.frontend.ingress[0].fqdn
+  description = "Frontend Container App public FQDN"
+}
